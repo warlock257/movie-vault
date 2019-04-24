6648dc00e711fd57fe0b84e9ca4ebd30115d5c06
David Courtemanche <davidcourtemanche@hotmail.com>
2019-04-23 23:51:44 -0400

    added dependencies to back, create react app in front

--- back-end/index.js
+++ back-end/index.js
@@ -4,20 +4,48 @@ const sequalize = new Sequelize("movies","postgres", process.env.dbPass, {
     "host": "localhost"
 });
 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const PORT = process.env.PORT || 8080;
+const axios = require('axios')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const cors = require('cors');
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
+
 //we are requiring in all the models in the models folder, and storing in variable db
 const db = require('./models')
 
-//create a new movie
-const blackPanther = {
-    title:"Black Panther 4K Ultra [Blu-ray]",
-    upc:"786936856316",
-    format:"uhd",
-    imgUrl:"https://d29pz51ispcyrv.cloudfront.net/images/I/J3u8uowQRzyQ7O6x1.MD256.JPEG"
-}
-db.movies.create(blackPanther)
-    .then(newMovie => {
-        console.log(newMovie);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+//create a new movie EXAMPLE
+// const blackPanther = {
+//     title:"Black Panther 4K Ultra [Blu-ray]",
+//     upc:"786936856316",
+//     format:"uhd",
+//     imgUrl:"https://d29pz51ispcyrv.cloudfront.net/images/I/J3u8uowQRzyQ7O6x1.MD256.JPEG"
+// }
+// db.movies.create(blackPanther)
+//     .then(newMovie => {
+//         console.log(newMovie);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+
+//add new movie by UPC
+
+//add new movie manually
+
+//query db
+
+//delete movie (from back end / postman only)
+
+
+
+app.listen(PORT, () =>{
+    console.log("Listening on port: " + PORT)
+})
