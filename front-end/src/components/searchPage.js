8c703ee21ffcb778eae4b2e97095016ddc757637
David Courtemanche <davidcourtemanche@hotmail.com>
2019-05-02 19:42:33 -0400

    more styling, added format logos

--- front-end/src/components/searchPage.js
+++ front-end/src/components/searchPage.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react'
 
 export default class searchPage extends Component {
 
-
-
-
   render() {
-
+    
     let titleResults = this.props.titleSearchResults.map((object, index) =>{
+      let discImg = ""
+      if(this.props.titleSearchResults[index].disc === 'dvd'){
+        discImg = "DVD-Logo.png"
+      } else if (this.props.titleSearchResults[index].disc === 'bd'){
+        discImg = "Blu_ray_logo.png"
+      } else if (this.props.titleSearchResults[index].disc === 'uhd'){
+        discImg = "4k-logo.png"
+      } else if (this.props.titleSearchResults[index].disc === 'digital'){
+        discImg = "play_logo.png"
+      }
       return(
+        <>
         <div className="singleDBResult">
-          <h3>{this.props.titleSearchResults[index].title}</h3>
-          <p>UPC: {this.props.titleSearchResults[index].upc}</p>
-          <p>Format: {this.props.titleSearchResults[index].disc}</p>
-          <img src={this.props.titleSearchResults[index].imgUrl} alt="movie poster"/>
-          <hr/>
+          <div className="singleDBResult__text">
+            <h3>{this.props.titleSearchResults[index].title}</h3>
+            <p>UPC: {this.props.titleSearchResults[index].upc}</p>
+            <img src={discImg} alt="format logo"/>
+          </div>
+
+          <div className="singleDBResult__pic">
+            <img src={this.props.titleSearchResults[index].imgUrl} alt="movie poster"/>
+          </div>
+          
       </div>
+      <hr/>
+      </>
       )
     })
 
@@ -25,8 +40,8 @@ export default class searchPage extends Component {
         <div className="searchArea">
           <h1>Search Movie Vault</h1>
           <form onSubmit={this.props.searchForTitle}>
-            <input type="text" name="searchDb" />
-            <button>Search</button>
+            <input type="text" name="searchDb" placeholder="search by title" />
+            <button type="submit">Search</button>
           </form>
         </div>
 
